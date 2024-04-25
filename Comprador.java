88836719961dfeaa7571b929b2efb97fd6943ab0
Bliclk <iperez2023@udec.cl>
2024-04-24 22:49:00 -0200

    Modificacion de Comprador para q maneje los throws

--- Comprador.java
+++ Comprador.java
@@ -2,18 +2,18 @@ package org.example;
 
 public class Comprador {
     private Expendedor expendedor;
-    private String sonido;
+    private String sabor;
     private int vuelto = 0;
 
-    public Comprador(Moneda m, int cual, Expendedor exp) {
+    public Comprador(Moneda m, int cual, Expendedor exp) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException, ProductoIncorrectoException{
         expendedor = exp;
-        Bebida bebida = exp.comprarBebida(m, cual);
+        Producto bebida = exp.comprarProducto(m, cual);
         for (Moneda i = expendedor.getVuelto(); i != null; i = expendedor.getVuelto()) {
             vuelto += i.getValor();
         }
         if (bebida == null) {
-            sonido = null;
-        } else sonido = bebida.beber();
+            sabor = null;
+        } else sabor = bebida.beber();
     }
 
     public int cuantoVuelto() {
@@ -21,7 +21,6 @@ public class Comprador {
     }
 
     public String queBebiste() {
-        return sonido;
+        return sabor;
     }
-
 }
