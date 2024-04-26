32478d5df30203c125d6b89baaab5a8a75d4390e
Gianpo164 <gmassoglia2023@udec.cl>
2024-04-25 22:05:46 -0400

    Implementacion del metodo compareTo

--- Moneda.java
+++ Moneda.java
@@ -1,13 +1,22 @@
 package org.example;
 
-public abstract class Moneda {
-    public Moneda() {
-
-    }
+public abstract class Moneda implements Comparable<Moneda> {
+    public Moneda() {}
 
     public Moneda getSerie() {
         return this;
     }
 
     public abstract int getValor();
+
+    public int compareTo(Moneda m) {
+        if (this.getValor() > m.getValor())
+            return 1;
+
+        else if (this.getValor() < m.getValor())
+            return -1;
+
+        else
+            return 0;
+    }
 }
